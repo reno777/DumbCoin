9dbdb4732ea39b1dd308488dd10c552c0f6f04dd
reno <srasmus2@emich.edu>
2018-12-09 16:48:04 -0500

    Step2 completed

--- Files/step2.py
+++ Files/step2.py
@@ -10,16 +10,114 @@
 ### [SCHN96], p.142 to implement an electronic cash system.
 """
 
-import time
+import pickle
 
-def testing2() :
+def blind() :
+
+    #bank vars
+    e=29
+    n=571
+    d=59
+
+    with open('/home/reno/git/DumbCoin/Files/output/k_variables.txt','rb') as f :
+        k1 = f.readline()
+        k2 = f.readline()
+        k3 = f.readline()
+        f.close()
+
+    #first money order.
+    with open('/home/reno/git/DumbCoin/Files/output/money_order1_output.txt','rb') as f :
+        money_order1 = pickle.load(f)
+
     print " "
-    print "[*] Step 2 complete!"
+    print "[!] - Money Order 1"
+    print "[*] - Amount:",money_order1[0]
+    print "[*] - Uniqueness Number:",money_order1[1]
+    print "[*] - I11:",money_order1[2],money_order1[3],money_order1[4],money_order1[5]
+    print "[*] - I12:",money_order1[6],money_order1[7],money_order1[8],money_order1[9]
     print " "
-    time.sleep(1)
+
+    blind_money_order1 = []
+    i = 0
+    while i < len(money_order1) :
+        blind_money_order1.append((money_order1[i] * (int(k1) ** e)) % n)
+        i += 1
+
+    print " "
+    print "[!] - Blinded Money Order 1"
+    print "[*] - Amount:",blind_money_order1[0]
+    print "[*] - Uniqueness Number:",blind_money_order1[1]
+    print "[*] - I11:",blind_money_order1[2],blind_money_order1[3],blind_money_order1[4],blind_money_order1[5]
+    print "[*] - I12:",blind_money_order1[6],blind_money_order1[7],blind_money_order1[8],blind_money_order1[9]
+    print " "
+
+    with open('/home/reno/git/DumbCoin/Files/output/blind_money_order1.txt','wb') as f :
+        pickle.dump(blind_money_order1, f)
+
+    #second money order
+    with open('/home/reno/git/DumbCoin/Files/output/money_order2_output.txt','rb') as f :
+        money_order2 = pickle.load(f)
+    
+    print " "
+    print "[!] - Money Order 2"
+    print "[*] - Amount:",money_order2[0]
+    print "[*] - Uniqueness Number:",money_order2[1]
+    print "[*] - I21:",money_order2[2],money_order2[3],money_order2[4],money_order2[5]
+    print "[*] - I22:",money_order2[6],money_order2[7],money_order2[8],money_order2[9]
+    print " "
+    
+    blind_money_order2 = []
+    i = 0
+    while i < len(money_order2) :
+        blind_money_order2.append((money_order2[i] * (int(k2) ** e)) % n)
+        i += 1
+    
+    print " "
+    print "[!] - Blinded Money Order 2"
+    print "[*] - Amount:",blind_money_order2[0]
+    print "[*] - Uniqueness Number:",blind_money_order2[1]
+    print "[*] - I21:",blind_money_order2[2],blind_money_order2[3],blind_money_order2[4],blind_money_order2[5]
+    print "[*] - I22:",blind_money_order2[6],blind_money_order2[7],blind_money_order2[8],blind_money_order2[9]
+    print " "
+    
+    with open('/home/reno/git/DumbCoin/Files/output/blind_money_order2.txt','wb') as f :
+        pickle.dump(blind_money_order2, f)
+
+    #third money order
+    with open('/home/reno/git/DumbCoin/Files/output/money_order3_output.txt','rb') as f :
+        money_order3 = pickle.load(f)
+    
+    print " "
+    print "[!] - Money Order 3"
+    print "[*] - Amount:",money_order3[0]
+    print "[*] - Uniqueness Number:",money_order3[1]
+    print "[*] - I31:",money_order3[2],money_order3[3],money_order3[4],money_order3[5]
+    print "[*] - I32:",money_order3[6],money_order3[7],money_order3[8],money_order3[9]
+    print " "
+    
+    blind_money_order3 = []
+    i = 0
+    while i < len(money_order3) :
+        blind_money_order3.append((money_order3[i] * (int(k3) ** e)) % n)
+        i += 1
+    
+    print " "
+    print "[!] - Blinded Money Order 3"
+    print "[*] - Amount:",blind_money_order3[0]
+    print "[*] - Uniqueness Number:",blind_money_order3[1]
+    print "[*] - I31:",blind_money_order3[2],blind_money_order3[3],blind_money_order3[4],blind_money_order3[5]
+    print "[*] - I32:",blind_money_order3[6],blind_money_order3[7],blind_money_order3[8],blind_money_order3[9]
+    print " "
+    
+    with open('/home/reno/git/DumbCoin/Files/output/blind_money_order3.txt','wb') as f :
+        pickle.dump(blind_money_order3, f)
+
+    print " "
+    print "[!!] - Money orders have been blinded!"
+    print " "
 
 def main2() :
-    testing2()
+    blind()
 
 if __name__ == "__main__" :
     main2()
