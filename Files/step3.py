6b4dec7b7c94a386549ad18e8771ecae6543e6aa
reno <srasmus2@emich.edu>
2018-12-09 20:12:04 -0500

    First money order unblind working

--- Files/step3.py
+++ Files/step3.py
@@ -10,17 +10,76 @@
 ### [SCHN96], p.142 to implement an electronic cash system.
 """
 
-import time
+import pickle
+from subprocess import call
 
-def testing3() :
+def unblind() :
+    #bank vars
+    e = 29
+    n = 571
+    d = 59
+
+    with open('Files/output/k_variables.txt','rb') as f :
+        k_values = pickle.load(f)
+
+    blindmo1(e, n, k_values[0])
+
+def large_num(e, n, k_value) :
+    with open('Files/perl_input.txt','w') as f :
+        print >> f, k_value
+        print >> f, "-1"
+        print >> f, n
+        f.close()
+
+    call(["perl","Files/LargeNumberCalc.pl"])
+
+    with open('Files/perl_output.txt','rb') as f :
+        invt = f.readline()
+        f.close()
+
+    with open('Files/perl_input.txt','w') as f :
+        print >> f, invt
+        print >> f, e
+        print >> f, n
+        f.close()
+
+    call(["perl","Files/LargeNumberCalc.pl"])
+    
+    with open('Files/perl_output.txt','rb') as f :
+        unblinding_factor = f.readline()
+        f.close()
+
+    return unblinding_factor
+
+def blindmo1(e, n, k1) :
+    #testing unblind for moneyorder1
+    with open('Files/output/blind_money_order1.txt','rb') as f :
+        blind_money_order1 = pickle.load(f)
+
     print " "
-    print "[*] Step 3 complete!"
+    print "[!] - Blinded Money Order 1"
+    print "[*] - Amount:",blind_money_order1[0]
+    print "[*] - Uniqueness Number:",blind_money_order1[1]
+    print "[*] - I11:",blind_money_order1[2],blind_money_order1[3],blind_money_order1[4],blind_money_order1[5]
+    print "[*] - I12:",blind_money_order1[6],blind_money_order1[7],blind_money_order1[8],blind_money_order1[9]
     print " "
-    time.sleep(1)
+
+    unblinding_factor = large_num(e, n, k1)
+
+    unblind_money_order1 = []
+    i = 0
+    while i < len(blind_money_order1) :
+        unblind_money_order1.append((blind_money_order1[i] * int(unblinding_factor)) % n)
+        i += 1
+
+    print " "
+    print "[!] - Unblinded Money Order 1"
+    print "[*] - Amount:",unblind_money_order1[0]
+    print "[*] - Uniqueness Number:",unblind_money_order1[1]
+    print " "
 
 def main3() :
-    testing3()
+    unblind()
 
 if __name__ == "__main__" :
     main3()
-    
